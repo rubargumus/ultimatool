a6b67d69de2b14fae4897d1a36b6207c40eb4981
Rubar  Gümüş <61204308+rubargumus@users.noreply.github.com>
2022-12-14 13:41:25 +0300

    Add files via upload

---
+++ kur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+sudo su 
+
+apt update -y 
+
+apt upgrade -y 
+
+apt install python3 -y 
+
+apt install figlet -y 
+
+apt install pip3 -y 
+
+pip3 install  requests 
+
+clear
+
+figlet IT's OK
